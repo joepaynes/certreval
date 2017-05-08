9c54c20f92870e4d5c65a656f029b9dc0e615e5c
Joseph Payne <payne.joe@hotmail.co.nz>
2017-05-08 20:30:59 +0000

    inital commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser");
+    // mongoose        = require("mongoose");
+
+
+//Setup    
+// mongoose.connect("mongodb://localhost/cert_reval");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+//Routes
+var indexRoutes     = require("./routes/index.js");
+
+app.use(indexRoutes);
+
+//Serve
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Serving Application");
+});
